324850c2876333c2e516afd80de4f18296a29dbd
ellie <ellie.tms1371@gmail.com>
2020-06-25 22:19:18 +0100

    add codes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 
 function App() {
+
+  const [Quote, setQuote] = useState({});
+  useEffect(() => newQuote(), []);
+  function newQuote() {
+    fetch("https://ellietms-code-server.glitch.me/quotes/random")
+      .then((response) => response.json())
+      .then((data) => setQuote(data));
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-12 mx-auto px-3 boxContainer">
+       <div className="img-1">
+       <img className="mb-2 img-1"
+        src="https://www.netclipart.com/pp/m/108-1084788_floral-flowers-flower-art-botanical-flowers-peony-flower.png"/>
+        <img className="mb-2 img-1"
+        src="https://www.netclipart.com/pp/m/108-1084788_floral-flowers-flower-art-botanical-flowers-peony-flower.png" />
+       </div>
+      <div className="my-4 mx-auto px-auto">
+      <i className="fas fa-book mx-2 my-auto"></i>
+        {Quote.quote}
+      </div>
+      <div className="my-4 mx-2 px-auto author">
+         author : {Quote.author}
+      </div>
+      <button
+      className="button"
+      onClick={() => newQuote()}>quote</button>
+      <div className="img-1">
+       <img className="mb-2 img-1"
+        src="https://www.netclipart.com/pp/m/108-1084788_floral-flowers-flower-art-botanical-flowers-peony-flower.png" />
+        <img className="mb-2 img-1"
+        src="https://www.netclipart.com/pp/m/108-1084788_floral-flowers-flower-art-botanical-flowers-peony-flower.png" />
+       </div>
+    </div>
+    </div>
     </div>
   );
 }
