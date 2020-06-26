96dd542e549225b9594b72f23a490131d9a82b9c
ellie <ellie.tms1371@gmail.com>
2020-06-26 13:57:50 +0100

    add changes

--- src/App.js
+++ src/App.js
@@ -16,19 +16,18 @@ function App() {
     <div className="container">
       <div className="row">
         <div className="col-12 mx-auto px-3 boxContainer">
-       <div className="img-1">
-       <img className="mb-2 img-1"
+       <div className="img-1 mb-4">
+       <img className="img-1 mb-4"
         src="https://www.pngkit.com/png/full/137-1376664_flower-art-decoupage-flowers-png.png"
         alt="flower"/>
       
-        <img className="mb-4 img-1"
+        <img className="img-1 mb-4"
         src="https://i.dlpng.com/static/png/1266549-flowers-photo-png-png-images-flower-png-400_400_preview.png" 
         alt="flower"/>
        </div>
       <div className="my-4 mx-auto px-auto">
       <i className="fas fa-book mx-2 my-auto"></i>
         {Quote.quote}
-      <i className="fas fa-book mx-2 my-auto"></i>
       </div>
       <div className="my-4 mx-2 px-auto author">
          Author : {Quote.author}
